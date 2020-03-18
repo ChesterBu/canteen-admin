c3e37d50d1dfa5419b6dc89c64ba5a3746f695cd
buyuxing <buyuxing@meituan.com>
2020-03-18 22:38:18 +0800

    tab

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,108 +1,100 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.less';
 import useRequest from '@umijs/use-request';
-import { Table, Tabs } from 'antd';
+import { Table, Tabs, Tag, Button } from 'antd';
 const { TabPane } = Tabs;
 
 
-const diningRoomColumns = [
+enum STATUS {
+  '正常' = 1,
+  '删除' = 2
+}
+enum STATUSCOLOR {
+  'success' = 1,
+  'error' = 2
+}
+
+const columns = [
   {
-    title: '食堂id',
-    dataIndex: 'id',
-    width: 250,
+    title: 'account',
+    dataIndex: 'account',
+    width: 200
   },
   {
-    title: '食堂名称',
-    dataIndex: 'name',
-    width: 250,
+    title: 'depentmentId',
+    dataIndex: 'depentmentId',
+    width: 200,
   },
   {
-    title: '食堂电话',
-    dataIndex: 'phone',
-    width: 250,
+    title: 'depent',
+    dataIndex: 'depent',
+    width: 200,
+    render: depent => <a>{ depent.name }</a>,
   },
   {
-    title: '负责人',
+    title: 'principalMan',
     dataIndex: 'principalMan',
-    width: 250,
-  }
+    width: 200,
+  },
+  {
+    title: 'status',
+    dataIndex: 'status',
+    width: 200,
+    render: status =>  <Tag color= { STATUSCOLOR[status]}>{ STATUS[status] }</Tag>
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    width: 200,
+    render: (text, record) => (
+      <span>
+        <a style={{ marginRight: 16 }}>详情</a>
+        { record.status === 1 && <a style={{ marginRight: 16 }}>删除</a> }
+        <a>修改</a>
+      </span>
+    ),
+  },
 ];
 
-const financesColumns = [
-  {
-    title: '财务部id',
-    dataIndex: 'id',
-    width: 250,
-  },
-  {
-    title: '财务部名称',
-    dataIndex: 'name',
-    width: 250,
-  },
-  {
-    title: '联系方式',
-    dataIndex: 'phone',
-    width: 250,
-  },
-  {
-    title: '负责人',
-    dataIndex: 'principalMan',
-    width: 250,
-  },
-]
 
-const suppliersColumns = [
-  {
-    title: '供应商id',
-    dataIndex: 'id',
-    width: 250,
-  },
-  {
-    title: '供应商名称',
-    dataIndex: 'name',
-    width: 250,
-  },
-  {
-    title: '手机号码',
-    dataIndex: 'phone',
-    width: 250,
-  },
-  {
-    title: '负责人',
-    dataIndex: 'principalMan',
-    width: 250,
-  },
-]
+const tables = ['diningRooms','finances','suppliers']
 
-const tables = [
-  {
-    dataSource: 'diningRooms',
-    columns: diningRoomColumns
-  },
-  {
-    dataSource: 'finances',
-    columns: financesColumns
-  },
-  {
-    dataSource: 'suppliers',
-    columns: suppliersColumns
-  },
-]
+const InfoPane = ({ i }) => {
+  const { data, run , loading } = useRequest((params) => ({
+    url: '/api/admin/account/all',
+    method: 'get',
+    params,
+  }),{
+    manual:true
+  })
+  useEffect(()=>{
+      run({ role: i + 1 })
+  },[])
+  return(
+    <>
+      <Button type="primary" style={{ float: 'right' }}>添加账户</Button>
+      <Table
+        rowKey={ (_,index) => index }
+        loading={ loading } 
+        columns={ columns }
+        dataSource = { data?.data } 
+      />
+    </>
+  )
+}
+
 
 const Home = () => {
-  const { data, loading } = useRequest('/api/admin/info')
+  
   return( 
     <div className="page-content">
       <Tabs defaultActiveKey="0" >
       {
         tables.map((v, i)=>(
-          <TabPane tab="Tab 0" key={ i.toString() }>
-            <Table
-              key = { i }
-              rowKey={ (_,index) => index }
-              loading={ loading } 
-              columns={ v.columns }
-              dataSource = { data?.data?.[v.dataSource] } 
+          <TabPane tab={ v } key={ i.toString() }>
+            <InfoPane 
+              key= { i }
+              i = { i }
             />
           </TabPane>
         ))
